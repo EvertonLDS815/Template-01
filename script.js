865f2024b6215e12a9fee9156feeb0df1e8ddf96
EvertonLDS815 <user.name>
2022-10-20 18:21:03 -0300

    Meu site de aprendizado responsivo

---
+++ script.js
@@ -0,0 +1,48 @@
+let menuBar = document.getElementById('im-4');
+let menuButton = document.getElementById('im-icon');
+let pageReturn = document.querySelector('.im-content-return');
+let buttonret = document.getElementById('button-return');
+
+
+const transitionMenu = () => {
+    if(menuBar.style.width === '200px') {
+        menuBar.style.width = '0';
+        pageReturn.style.display = 'none';
+    } else {
+        returnMenu();
+    }
+}
+/* menuButton.addEventListener('click', transitionMenu) */
+const returnMenu = () => {
+    menuBar.style.width = '200px';
+    pageReturn.style.display = 'block';
+}
+pageReturn.addEventListener('click', transitionMenu)
+
+const buttonReturn = () => {
+    window.scrollTo(0, 0);
+    console.log(name)
+}
+
+//animation-button-scroll
+
+let elem = document.getElementById('perc')
+let doc = document.documentElement
+
+window.addEventListener('scroll', function() {
+    let value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight))
+    elem = value + '%'
+    /* console.log(doc.scrollTop) */
+
+    if (value < 35) {
+        buttonret.style.visibility = 'hidden';
+        buttonret.style.opacity = '0';
+    } else {
+        buttonret.style.visibility = 'visible';
+        buttonret.style.opacity = '1';
+    }
+})
+
+const topScroll = () => {
+    window.scrollTo(0, 0);
+}
